357d6751101cb5ffe84ada0dd94d6928cdb09164
Felipe Oliveira <flpoliveira42@gmail.com>
2019-03-30 18:44:45 +0000

    Ajustes dos desafios: Importado corretamente o logo em svg; a pasta imagem foi colocada dentro do src

--- app/src/components/LogoBotcamp/index.js
+++ app/src/components/LogoBotcamp/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import "./style.css"
+
+import "./style.css";
+import logo from "../../img/botcamp.svg";
 
 const LogoBotcamp = () => (
-  <img className="logo-botcamp" src="./img/botcamp.png" alt="Logo do Botcamp" title="Botcamp" />
+  <img className="logo-botcamp" src={logo} alt="Logo do Botcamp" title="Botcamp" />
 );
 
 export default LogoBotcamp;
