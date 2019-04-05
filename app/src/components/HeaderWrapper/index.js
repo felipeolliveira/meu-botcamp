d52b0327857e85a5cf05843787d9c0c9cedebc2d
Felipe Oliveira <flpoliveira42@gmail.com>
2019-04-05 20:59:27 +0000

    Aula 04 - Desafio intermediário

--- app/src/components/HeaderWrapper/index.js
+++ app/src/components/HeaderWrapper/index.js
@@ -5,6 +5,21 @@ const HeaderWrapper = styled.header`
   width: 100%;
   background-color: #26265e;
   padding: 10px 16px;
+  position: relative;
+  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.25);
+
+  &:after {
+    content: "";
+    position: absolute;
+    width: 0;
+    height: 0;
+    bottom: 0;
+    right: 0;
+    border-style: solid;
+    border-width: 0 400px 5px 0;
+    border-color: transparent #26265e transparent transparent;
+    transform: translateY(100%);
+  }
 `;
 
 export default HeaderWrapper;
